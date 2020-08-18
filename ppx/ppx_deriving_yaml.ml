7d0863f6b4abe2113397f62cd798cdae04503168
patricoferris <patrickferris17@gmail.com>
2020-08-18 10:51:30 +0100

    remove concat_map for backwards compatibility

--- ppx/ppx_deriving_yaml.ml
+++ ppx/ppx_deriving_yaml.ml
@@ -12,51 +12,54 @@ let mangle_name_label suff label =
 
 let generate_impl ~ctxt (_rec_flag, type_decls) =
   let loc = Expansion_context.Deriver.derived_item_loc ctxt in
-  List.concat_map
-    (fun typ_decl ->
-      match typ_decl with
-      | { ptype_kind = Ptype_abstract; ptype_manifest; ptype_name; _ } -> (
-          match ptype_manifest with
-          | Some t ->
-              let yamliser =
-                Ppx_deriving.poly_fun_of_type_decl typ_decl
-                  [%expr [%e Value.type_to_expr t]]
-              in
-              let ocamliser =
-                Ppx_deriving.poly_fun_of_type_decl typ_decl
-                  (Value.of_yaml_type_to_expr None t)
-              in
-              let to_yaml = mangle_name_label suf_to ptype_name.txt in
-              let of_yaml = mangle_name_label suf_of ptype_name.txt in
-              [
-                pstr_value ~loc Nonrecursive
-                  [ Vb.mk (ppat_var ~loc { loc; txt = to_yaml }) yamliser ];
-                pstr_value ~loc Nonrecursive
-                  [ Vb.mk (ppat_var ~loc { loc; txt = of_yaml }) ocamliser ];
-              ]
-          | None ->
-              Location.raise_errorf ~loc "Cannot derive anything for this type")
-      | { ptype_kind = Ptype_record fields; ptype_loc; ptype_name; _ } ->
-          let to_yaml = mangle_name_label suf_to ptype_name.txt in
-          let of_yaml = mangle_name_label suf_of ptype_name.txt in
-          [
-            pstr_value ~loc Nonrecursive
-              [
-                Vb.mk
-                  (ppat_var ~loc { loc; txt = to_yaml })
-                  (Ppx_deriving.poly_fun_of_type_decl typ_decl
-                     (Value.record_to_expr ~loc:ptype_loc fields));
-              ];
-            pstr_value ~loc Nonrecursive
-              [
-                Vb.mk
-                  (ppat_var ~loc { loc; txt = of_yaml })
-                  (Ppx_deriving.poly_fun_of_type_decl typ_decl
-                     (Value.of_yaml_record_to_expr ~loc:ptype_loc fields));
-              ];
-          ]
-      | _ -> Location.raise_errorf ~loc "Cannot derive anything for this type")
-    type_decls
+  List.concat
+    (List.map
+       (fun typ_decl ->
+         match typ_decl with
+         | { ptype_kind = Ptype_abstract; ptype_manifest; ptype_name; _ } -> (
+             match ptype_manifest with
+             | Some t ->
+                 let yamliser =
+                   Ppx_deriving.poly_fun_of_type_decl typ_decl
+                     [%expr [%e Value.type_to_expr t]]
+                 in
+                 let ocamliser =
+                   Ppx_deriving.poly_fun_of_type_decl typ_decl
+                     (Value.of_yaml_type_to_expr None t)
+                 in
+                 let to_yaml = mangle_name_label suf_to ptype_name.txt in
+                 let of_yaml = mangle_name_label suf_of ptype_name.txt in
+                 [
+                   pstr_value ~loc Nonrecursive
+                     [ Vb.mk (ppat_var ~loc { loc; txt = to_yaml }) yamliser ];
+                   pstr_value ~loc Nonrecursive
+                     [ Vb.mk (ppat_var ~loc { loc; txt = of_yaml }) ocamliser ];
+                 ]
+             | None ->
+                 Location.raise_errorf ~loc
+                   "Cannot derive anything for this type")
+         | { ptype_kind = Ptype_record fields; ptype_loc; ptype_name; _ } ->
+             let to_yaml = mangle_name_label suf_to ptype_name.txt in
+             let of_yaml = mangle_name_label suf_of ptype_name.txt in
+             [
+               pstr_value ~loc Nonrecursive
+                 [
+                   Vb.mk
+                     (ppat_var ~loc { loc; txt = to_yaml })
+                     (Ppx_deriving.poly_fun_of_type_decl typ_decl
+                        (Value.record_to_expr ~loc:ptype_loc fields));
+                 ];
+               pstr_value ~loc Nonrecursive
+                 [
+                   Vb.mk
+                     (ppat_var ~loc { loc; txt = of_yaml })
+                     (Ppx_deriving.poly_fun_of_type_decl typ_decl
+                        (Value.of_yaml_record_to_expr ~loc:ptype_loc fields));
+                 ];
+             ]
+         | _ ->
+             Location.raise_errorf ~loc "Cannot derive anything for this type")
+       type_decls)
 
 let generate_intf ~ctxt (_rec_flag, type_decls) : Ppxlib.Ast.signature_item list
     =
