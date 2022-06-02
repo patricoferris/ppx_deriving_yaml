7714e168797a36ed093a2b47b2b428dd6a57dbda
Patrick Ferris <patrick@sirref.org>
2022-06-02 12:25:17 +0100

    Provide to_yaml and of_yaml with yaml being an alias (#36)

--- test/expect/passing/simple.ml
+++ test/expect/passing/simple.ml
@@ -1,1 +1,3 @@
 type t = { name : string; age : int option } [@@deriving yaml]
+type u = { name : string [@default "Una"] } [@@deriving to_yaml]
+type v = { age : int [@key "AGE"] [@default 10] } [@@deriving of_yaml]
