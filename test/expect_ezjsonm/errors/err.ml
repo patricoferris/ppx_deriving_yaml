79bde82094a56790c89adeff77021b4a30b3fc7f
Patrick Ferris <patrick@sirref.org>
2024-11-17 18:10:02 +0000

    Separate tests

--- test/expect_ezjsonm/errors/err.ml
+++ test/expect_ezjsonm/errors/err.ml
@@ -1,7 +1,7 @@
-type t = int -> int [@@deriving yaml]
-type u = int -> int [@@deriving yaml]
-type v = int [@@deriving yaml]
-type w = .. [@@deriving yaml]
-type x [@@deriving yaml]
+type t = int -> int [@@deriving ezjsonm]
+type u = int -> int [@@deriving ezjsonm]
+type v = int [@@deriving ezjsonm]
+type w = .. [@@deriving ezjsonm]
+type x [@@deriving ezjsonm]
 
-let _ = v_of_yaml
+let _ = v_of_ezjsonm
